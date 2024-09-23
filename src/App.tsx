ca5133ba8918dc3c188dca3abcf6a75bbbe1721e
Asef00 <asefhosseini00@gmail.com>
2024-09-23 14:45:18 +0330

    remove unneseccary state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 
 function App() {
-  const [player1, setPlayer1] = useState('Player 1')
-  const [player2, setPlayer2] = useState('Player 2')
-
   return (
     <main>
       <div className="rounded-lg bg-black p-5 mb-10">
         {/* players */}
         <ol className="flex gap-10 justify-center mb-10">
-          <Player initialName={player1} symbol="X" />
-          <Player initialName={player2} symbol="O" />
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         {/* game board */}
         <GameBoard />
